4c8fef4eff1dedb540478e42ec3ef01cdca8d6bb
John Mark Redding <johomurk@gmail.com>
2018-12-04 15:01:20 -0500

    Add create_account to User

--- lib/user.rb
+++ lib/user.rb
@@ -17,9 +17,31 @@ class User < ActiveRecord::Base
           puts "Enter your password. "
           password = STDIN.gets.chomp
           logged_in = current_user.password == password
+          if logged_in
+            puts "Welcome to Osumazon!"
+          else
+            puts "Wrong password"
+          end
         else
           puts "Invalid username."
         end
+      end
+    end
+  end
+
+  def self.create_account
+    created = false
+    until created
+      puts "Enter your new username (Can’t be 'exit'): "
+      username = STDIN.gets.chomp
+      if !User.find_by(username: username)
+        puts "Enter your new password: "
+        password = STDIN.gets.chomp
+        User.create(username: username, password: password)
+        puts "Your new account details. Username: #{username}, Password: #{password}."
+        created = true
+      else
+        puts "Username already chosen!"
       end
     end
   end
