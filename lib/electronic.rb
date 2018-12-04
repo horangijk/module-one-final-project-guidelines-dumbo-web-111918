523edc6fd0273ba7bb87037957e1c94a9a3ec124
John Mark Redding <johomurk@gmail.com>
2018-12-04 17:04:54 -0500

    create shopping menu

--- lib/electronic.rb
+++
@@ -1,8 +0,0 @@
-class Electronic < ActiveRecord::Base
-  has_many :transactions
-  has_many :users, through: :transactions
-
-  def self.categories
-    Electronic.all.map {|electronic| electronic.category }
-  end
-end
