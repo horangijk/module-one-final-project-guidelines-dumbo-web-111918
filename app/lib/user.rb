8212a61ee5d981422ab951610213ee8bde001bbd
John Mark Redding <johomurk@gmail.com>
2018-12-11 15:48:03 -0500

    Refactor

--- app/lib/user.rb
+++ app/lib/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :transactions
   has_many :electronics, through: :transactions
 
+  @@prompt = TTY::Prompt.new
+
   def display_transactions
     self.reload()
     if self.transactions.empty?
@@ -16,47 +18,66 @@ class User < ActiveRecord::Base
     end
   end
 
-  def delete_account
-    prompt = TTY::Prompt.new
-    deleted = false
-    return_val = false
-    until deleted
-      if prompt.select('Are you sure you want to delete your account?', ["Yes", "No"]) == "Yes"
-        delete_account_password = prompt.mask("Type in your password: ")
-        if delete_account_password == $current_user.password
-          self.destroy
-          exit = true
-          system "clear"
-          puts "Sorry to see you go! :("
-          sleep(1)
-          deleted = true
-          return_val = true
-        else
-          system "clear"
-          puts "Wrong password."
-          sleep(1)
-          deleted = false
+  def self.create_account
+    new_user = nil
+    done = false
+    until new_user || done
+      username = @@prompt.ask("Enter your new username (can’t be 'exit'): ")
+      system "clear"
+      if username == "exit"
+        puts "Well, we told you, it can’t be 'exit'"
+        if @@prompt.select("Would you like to exit?", ["Yes", "No"]) == "Yes"
+          done = true
         end
+      elsif !self.find_by(username: username)
+        password = @@prompt.mask("Enter your new password: ")
+        new_user = User.create(username: username, password: password)
+        puts "Your new account details. Username: #{username}, Password: #{password}."
       else
-        deleted = true
+        puts "Username already chosen!"
       end
     end
-    return_val
+    new_user
+  end
+
+  def delete_account()
+    done = false
+    until !self || done
+      if @@prompt.select('Are you sure you want to delete your account?', ["Yes", "No"]) == "Yes"
+        password = @@prompt.mask("Type in your password: ")
+        system "clear"
+        if password == self.password
+          self.destroy
+          exit = true
+          puts "Sorry to see you go! :("
+          sleep(2)
+        else
+          puts "Wrong password."
+          sleep(2)
+        end
+      else
+        done = true
+      end
+    end
+    !self
   end
 
   def change_password
-    prompt = TTY::Prompt.new
-    if prompt.select('Are you sure you want to update your password?', ["Yes", "No"]) == "Yes"
-      password = prompt.mask("Type in your new password: ")
-      if password == self.password
+    if @@prompt.select('Are you sure you want to update your password?', ["Yes", "No"]) == "Yes"
+      if self.password == @@prompt.ask("Enter old password: ")
+        password = @@prompt.mask("Type in your new password: ")
         system "clear"
-        puts "Cannot change password. Cannot use the same password."
-        sleep(2)
+        if password == self.password
+          puts "Cannot change password. Cannot use the same password."
+          sleep(2)
+        else
+          self.password = password
+          self.save
+          puts "Password updated."
+          sleep(2)
+        end
       else
-        self.password = password
-        self.save
-        system "clear"
-        puts "Password updated."
+        "You don’t have permission!"
         sleep(2)
       end
     end
--- app/osumazon.rb
+++ app/osumazon.rb
@@ -1,235 +1,209 @@
-$current_user = nil
-$cart = []
 
-def login
-  prompt = TTY::Prompt.new
-  logged_in = false
-  exit = false
-  until logged_in || exit
-    puts "Enter your username, or type 'exit': "
-    username = STDIN.gets.chomp
-    if username == 'exit'
-      exit = true
-      system "clear"
-      puts "Exiting!"
-      sleep(1)
+class App
+  @@current_user = nil
+  @@cart = []
+  @@prompt = TTY::Prompt.new
 
-    else
-      current_user = User.find_by(username: username)
-      if current_user
+  def self.invalid_cart_message
+    puts "Your cart is empty."
+    puts "Go get some shit!"
+  end
+
+  def self.login
+    done = false
+    until @@current_user || done
+      puts "Enter your username, or type 'exit': "
+      username = STDIN.gets.chomp
+      if username == 'exit'
+        done = true
         system "clear"
-        password = prompt.mask("Type in your password: ")
-        logged_in = current_user.password == password
-        if logged_in
-          puts "Welcome to Osumazon!"
-        else
-          puts "Wrong password"
-        end
-        $current_user = current_user
+        puts "Exiting!"
+        sleep(1)
       else
-        system "clear"
-        puts "Invalid username."
-      end
-    end
-  end
-  logged_in
-end
-
-def create_account
-  prompt = TTY::Prompt.new
-  created = false
-  until created
-    puts "Enter your new username (Can’t be 'exit'): "
-    username = gets.chomp
-    if username == "exit"
-      if prompt.select("Are you sure you want to exit?", ["Yes", "No"]) == "Yes"
-        system "clear"
-        created = true
-      else
-        system "clear"
-        puts "We told you, it can’t be 'exit'"
-      end
-    elsif !User.find_by(username: username)
-      password = prompt.mask("Type in your password: ")
-      User.create(username: username, password: password)
-      puts "Your new account details. Username: #{username}, Password: #{password}."
-      created = true
-    else
-      system "clear"
-      puts "Username already chosen!"
-    end
-  end
-end
-
-def add_to_cart(product)
-  $cart << product
-end
-
-def cart
-  $cart
-end
-
-def review_cart
-  choices = {}
-  $cart.each_with_index do |item, i|
-    choices["#{i+1}. #{item.name}"] = i
-  end
-  prompt = TTY::Prompt.new
-  indices_to_delete = prompt.multi_select("Press 'SPACE' to choose items to remove. Press 'ENTER' to continue.", choices)
-  indices_to_delete.each do |index|
-    if prompt.select("Are you sure you want to delete #{$cart[index].name}?", ["Yes", "No"]) == "Yes"
-      puts "Deleted #{$cart[index].name}."
-      $cart.delete_at(index)
-    end
-  end
-end
-
-def menu
-  prompt = TTY::Prompt.new
-  options = ["Shop", "Checkout", "Prior Purchases", "Settings", "Logout"]
-  logged_out = false
-
-  while $current_user
-    choice = prompt.select("What would you like to do? ", options)
-    case choice
-    when "Shop"
-      system "clear"
-      shop
-      system "clear"
-    when "Checkout"
-      system "clear"
-      if $cart.empty?
-        puts "Your cart is empty."
-        puts "Go get some shit!"
-      else
-        system "clear"
-        review_cart
-        system "clear"
-        if $cart.empty?
-          puts "Your cart is empty."
-          puts "Go get some shit!"
-        else
-          checkout
+        @@current_user = User.find_by(username: username)
+        if @@current_user
           system "clear"
+          password = @@prompt.mask("Type in your password: ")
+          if @@current_user.password == password
+            puts "Welcome to Osumazon!"
+          else
+            puts "Wrong password"
+            @@current_user = nil
+          end
+        else
+          system "clear"
+          puts "Invalid username."
         end
       end
-    when "Logout"
-      system "clear"
-      logout
-      system "clear"
-    when "Settings"
-      system "clear"
-      settings
-      system "clear"
-    when "Prior Purchases"
-      system "clear"
-      $current_user.display_transactions
-      system "clear"
     end
+    @@current_user
   end
-end
 
-def logout
-  system "clear"
-  puts "You are logged out."
-  sleep(1)
-  $current_user = nil
-  $cart = []
-end
-
-def checkout
-  sum = 0.0
-  $cart.each do |item|
-    sum += item.price
+  def self.add_to_cart(product)
+    @@cart << product
   end
-  system "clear"
-  puts "Your cart is $#{'%.2f' % sum}."
-  prompt = TTY::Prompt.new
-  if prompt.select("Do you wish to checkout?", ["Yes", "No"]) == "Yes"
-    $cart.each do |item|
-      new_trans = Transaction.find_or_create_by(user_id: $current_user.id , electronic_id: item.id)
-      new_trans.quantity ||= 0
-      new_trans.quantity += 1
-      new_trans.save
+
+  def self.review_cart
+    choices = {}
+    @@cart.each_with_index do |item, i|
+      choices["#{i+1}. #{item.name}"] = i
     end
-    $cart = []
-    puts "You spent $#{'%.2f' % (sum * 1.08875)}."
-    sleep(1)
-    print "Press any key to continue: "
-    STDIN.getch
-  else
-    puts "Okay, bye!"
-  end
-end
-
-def shop
-  complete = false
-  categories = Electronic.categories.uniq
-  categories << "exit"
-  prompt = TTY::Prompt.new
-
-  until complete
-    system "clear"
-    category = prompt.select("Select a category: ", categories)
-    if category == "exit"
-      complete = true
-    else
-      product_names = Electronic.where(category: category).map {|p| p.name}
-      product_names << "exit"
-      product_name = prompt.select("Select an item: ", product_names)
-      if product_name != "exit"
-        product = Electronic.find_by(name: product_name)
-        print "$#{'%.2f' % product.price} "
-        if prompt.select('Add to cart?', ["Yes", "No"]) == "Yes"
-          add_to_cart(product)
-        end
+    indices_to_delete = @@prompt.multi_select("Press 'SPACE' to choose items to remove. Press 'ENTER' to continue.", choices)
+    indices_to_delete.each do |index|
+      if @@prompt.select("Are you sure you want to delete #{@@cart[index].name}?", ["Yes", "No"]) == "Yes"
+        puts "Deleted #{@@cart[index].name}."
+        @@cart.delete_at(index)
       end
-      category = nil
-      product_name = nil
     end
   end
-end
 
-def start
-  quit = false
-  choices = ["Login", "Create Account", "Quit"]
-  prompt = TTY::Prompt.new
-  until quit
-    system "clear"
-    choice = prompt.select("Welcome to Osumazon! What would you like to do?", choices)
-    case choice
-    when "Login"
-      system "clear"
-      if login
+  def self.menu
+    options = ["Shop", "Checkout", "Prior Purchases", "Settings", "Logout"]
+    logged_out = false
+
+    while @@current_user
+      choice = @@prompt.select("What would you like to do? ", options)
+      case choice
+      when "Shop"
         system "clear"
-        menu
+        shop
+        system "clear"
+      when "Checkout"
+        system "clear"
+        if @@cart.empty?
+          invalid_cart_message
+        else
+          review_cart
+          system "clear"
+          if @@cart.empty?
+            invalid_cart_message
+          else
+            checkout
+            system "clear"
+          end
+        end
+      when "Logout"
+        system "clear"
+        logout
+        system "clear"
+      when "Settings"
+        system "clear"
+        settings
+        system "clear"
+      when "Prior Purchases"
+        system "clear"
+        @@current_user.display_transactions
+        system "clear"
       end
-    when "Create Account"
-      create_account
-    when "Quit"
-      system "clear"
-      puts "Bye bye from Osumazon!"
-      quit = true
     end
   end
-end
 
-def settings
-  prompt = TTY::Prompt.new
-  completed = false
-  options = ["exit", "Change Password", "Delete Account"]
-  until completed || !$current_user
+  def self.logout
     system "clear"
-    option = prompt.select("Select from the following: ", options)
-    case option
-    when "Change Password"
-      $current_user.change_password
-    when "Delete Account"
-      if $current_user.delete_account
-        logout
+    puts "You are logged out."
+    sleep(1)
+    @@current_user = nil
+    @@cart = []
+  end
+
+  def self.checkout
+    sum = 0.0
+    @@cart.each do |item|
+      sum += item.price
+    end
+    system "clear"
+    puts "Your cart is $#{'%.2f' % sum}."
+    if @@prompt.select("Do you wish to checkout?", ["Yes", "No"]) == "Yes"
+      @@cart.each do |item|
+        new_trans = Transaction.find_or_create_by(user_id: @@current_user.id , electronic_id: item.id)
+        new_trans.quantity ||= 0
+        new_trans.quantity += 1
+        new_trans.save
       end
-    when "exit"
-      completed = true
+      @@cart = []
+      puts "You spent $#{'%.2f' % (sum * 1.08875)}."
+      sleep(1)
+      print "Press any key to continue: "
+      STDIN.getch
+    else
+      puts "Okay, bye!"
+    end
+  end
+
+  def self.shop
+    complete = false
+    categories = Electronic.categories.uniq
+    categories << "exit"
+
+    until complete
+      system "clear"
+      category = @@prompt.select("Select a category: ", categories)
+      if category == "exit"
+        complete = true
+      else
+        product_names = Electronic.where(category: category).map {|p| p.name}
+        product_names << "exit"
+        product_name = @@prompt.select("Select an item: ", product_names)
+        if product_name != "exit"
+          product = Electronic.find_by(name: product_name)
+          print "$#{'%.2f' % product.price} "
+          if @@prompt.select('Add to cart?', ["Yes", "No"]) == "Yes"
+            add_to_cart(product)
+          end
+        end
+        category = nil
+        product_name = nil
+      end
+    end
+  end
+
+  def self.menu_start(message, options)
+    system "clear"
+    done = false
+    until done
+      choice = @@prompt.select(message, options)
+      system "clear"
+      done = yield choice
+    end
+  end
+
+  def self.start
+    welcome_message = "Welcome to Osumazon! What would you like to do?"
+    choices = ["Login", "Create Account", "Quit"]
+    self.menu_start(welcome_message, choices) do |choice|
+      case choice
+      when "Login"
+        if self.login
+          self.menu
+        end
+      when "Create Account"
+        @@current_user = User.create_account
+        system "clear"
+        @@current_user
+      when "Quit"
+        puts "Bye, bye from Osumazon!"
+        sleep(2)
+        true
+      end
+    end
+  end
+
+  def self.settings
+    settings_message = "Settings"
+    options = ["Change Password", "Delete Account", "<< Back"]
+    self.menu_start(settings_message, options) do |choice|
+      case choice
+      when "Change Password"
+        @@current_user.change_password
+        false
+      when "Delete Account"
+        if @@current_user.delete_account
+          self.logout
+          true
+        end
+      when "<< Back"
+        true
+      end
     end
   end
 end
